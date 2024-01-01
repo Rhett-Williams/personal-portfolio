beac8b62ddd25a81bae57e7c712f0cdf2cd68d72
rhett-williams <rhettjwilliams@gmail.com>
2024-01-01 13:05:32 +1000

    qwe

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -6,7 +6,7 @@ import "./about.css";
 import Profile from "../../assets/img/profile/profile.svg";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
-import Resume from "../../assets/pdf/Resume-of-Rhett-Williams.pdf"
+import Resume from "../../assets/pdf/Resume-of-Rhett-Williams.pdf";
 
 const About = () => {
   return (
@@ -17,14 +17,26 @@ const About = () => {
           <Row className=" pb-5 align-items-center">
             <Col xs={12} md={6}>
               <Row className="justify-content-center mb-2 mr-2 ">
-                <Image className="profile justify-content-end" alt="profile" src={Profile} />
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                />
               </Row>
             </Col>
             <Col xs={12} md={6}>
-              <Row className=" align-items-start p-2 my-details rounded tex align-text-left " >
-                <br />I am a skilled frontend developer with a passion for creating intuitive and visually appealing user experiences. My expertise lies in using modern technologies such as TypeScript, Kotlin, React, and React Native to build high-quality web and mobile applications.
-                <br /><br/>
-                I have a strong understanding of frontend development best practices and am comfortable working in agile development environments. In addition to my technical skills, I am a team player with excellent communication and problem-solving abilities.
+              <Row className=" align-items-start p-2 my-details rounded tex align-text-left ">
+                <br />
+                Highly motivated software engineer with a 3-year track record of
+                building engaging and efficient web applications, wielding the
+                full stack power of NextJS, React, and NodeJS. Adept at
+                architecting intuitive user interfaces, crafting robust backend
+                APIs, and optimizing performance for a seamless user experience.
+                Proven ability to thrive in both independent and collaborative
+                environments, bringing creative problem-solving skills and a
+                passion for technical innovation to every project. Eager to
+                leverage expertise and unwavering dedication to contribute to a
+                fast-paced team dedicated to building the future of software.
                 <br />
                 <Col className="d-flex justify-content-center flex-wrap">
                   <div>
@@ -42,14 +54,22 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://github.com/Rhett-Williams" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://github.com/Rhett-Williams"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-dark">
                         GitHub
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="https://www.linkedin.com/in/rhett-williams-527144220/" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://www.linkedin.com/in/rhett-williams-527144220/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Button className="m-2" variant="outline-info">
                         LinkedIn
                       </Button>
